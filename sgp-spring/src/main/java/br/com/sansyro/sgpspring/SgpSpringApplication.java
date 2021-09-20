b1b297b35e4bcfd684f13c41fabf64e7d2ff1720
alessandra <alessandrasilvav@gmail.com>
2021-09-20 11:22:42 -0300

    Adicionando projetos spring e quarkus

---
+++ sgp-spring/src/main/java/br/com/sansyro/sgpspring/SgpSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.sansyro.sgpspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgpSpringApplication.class, args);
+    }
+
+}
---
+++ sgp-spring/src/test/java/br/com/sansyro/sgpspring/SgpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sansyro.sgpspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
